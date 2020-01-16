e3055f59c59210977211c60a63b50728957b7d12
david v perekopskiy <david.perekopskiy@go.shoreline.edu>
2020-01-16 01:54:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -25,7 +25,12 @@ return source_array
 end
 
 def map_to_square(source_array)
-
+i=0
+while i<source_array.length do
+source_array[i]=source_array[i]**2
+i+=1
+end
+return source_array
 end
 def reduce_to_total(source_array, starting_point)
 
