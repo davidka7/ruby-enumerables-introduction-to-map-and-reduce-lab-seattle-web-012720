891aec60a95f12fa85223fd9f44854b46daf8e44
david v perekopskiy <david.perekopskiy@go.shoreline.edu>
2020-01-16 01:54:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -32,12 +32,15 @@ i+=1
 end
 return source_array
 end
+
 def reduce_to_total(source_array, starting_point)
 
 end
+
 def reduce_to_all_true(source_array)
 
 end
+
 def reduce_to_any_true(source_array)
 
 end
